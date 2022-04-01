ef42b0dc547371a2ad2e6063e188659dd27bc8fd
Ayberk <ybrk01@gmail.com>
2022-04-01 15:29:31 +0300

    first commit
    01-04-2022

---
+++ net-core-api-push-notification-demo/Models/FcmNotificationSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_api_push_notification_demo.Models
+{
+    public class FcmNotificationSetting
+    {
+        public string SenderId { get; set; }
+        public string ServerKey { get; set; }
+
+    }
+}
